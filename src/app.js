7e956519810266ce1bf1d3a6400cd35523f37a1f
wolfmarcos <estacread1@gmail.com>
2021-09-13 07:11:35 -0300

    v3

--- src/app.js
+++ src/app.js
@@ -1,56 +1,99 @@
-const express = require('express');
-const morgan = require ('morgan');
-const multer = require('multer');
-const path = require('path');
-const exphbs = require('express-handlebars');
-//const session = require('express-session');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
+
+const multer = require("multer");
 //const validator = require('express-validator');
-//const passport = require('passport');
-//const flash = require('connect-flash');
+const passport=require("passport");
 // const MySQLStore = require('express-mysql-session')(session);const bodyParser = require('body-parser');
-
 // Inicializando
-const app = express(); 
-require('./database');//lebanta mongoos
-//require('./lib/passport');
 
+require("./database");
+require('passport-local') 
+require("./config/passport");
+//app.set("port", config.PORT);
+//require('./config/passport')(passport);
+//require("./libs/createUser");
+// createAdminUser();
+const app = express();
 // Seteos
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 4000);
 //app.set('port', 3000);//indica el puerto
-app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({
-     defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'),'partials'),
-    extname: '.hbs',
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
     //helpers: require('./lib/handlebars')
-}));
-app.set('view engine', '.hbs');
-
+  })
+);
+app.set("view engine", ".hbs");
 //Middlewares
-app.use(morgan('dev'));//mostrar mensajes cortos por consola
-app.use(express.json());//json
+app.use(morgan("dev")); //mostrar mensajes cortos por consola
+app.use(express.json()); //json
+// static files
+app.use(express.static(path.join(__dirname, "public")));
 
-
-
-// // app.use(bodyParser.urlencoded({extended: false}));
-// // app.use(bodyParser.json());//interpreta estenciones Json
-
-// //app.use(multer().single('image'))//identifica si es una imagen que se envia al server antraves de 'image' para asi colocarla
-// /*
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, 'public/uploads'),
-    filename: (req, file, cb) => {
-        cb(null, new Date().getTime() + path.extname(file.originalname));
-    }
+  destination: path.join(__dirname, "  /uploads"),
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + path.extname(file.originalname));
+  },
 });
-app.use(multer({storage}).single('image'));
-app.use(require('./routes')); 
+app.use(multer({ storage }).single("image"));
+app.use(require("./routes"));
 // identifica si es una umagen la que se sube al server -multer cual es el campo que analiza
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use(express.urlencoded({extended: false}));//entiende datos desde un formulario//extended:false para que solo entienda txto
-// app.use(require('./routes')); 
+app.use(express.urlencoded({ extended: false })); //entiende datos desde un formulario//extended:false para que solo entienda txto
+
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+    store: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+// Global Variables
+
+// Global Variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+// // Routes
+
+app.use(require("./routes/index.js")); // entrega la ruta de la pagina web
+//app.use(require('./routes/authentication'));
+// //app.use('/links', require('./routes/links'));
+
+app.use(require("./routes/users.routes"));
+
+
+
+app.use((req, res) => {
+  res.render("404");
+});
+module.exports = app; // al exportar app puede utlizar  toda la vicion del app ya que el mismo app es express
+// app.use(require('./routes'));
 // // app.use(session({
 // //     secret: 'faztmysqlnodemysql',
 // //     resave: false,
@@ -60,12 +103,10 @@ app.use(express.urlencoded({extended: false}));//entiende datos desde un formula
 // /*
 // app.use(passport.initialize());
 // app.use(passport.session());
-// app.use(session({ cookie: { maxAge: 60000 }, 
+// app.use(session({ cookie: { maxAge: 60000 },
 //   secret: 'woot',
-//   resave: false, 
+//   resave: false,
 //   saveUninitialized: false}));
-
-// app.use(flash());
 
 // // Global variables
 // app.use((req, res, next) => {
@@ -75,11 +116,8 @@ app.use(express.urlencoded({extended: false}));//entiende datos desde un formula
 //     next();
 //   });
 // */
-// // Routes
-const ruta= require('./routes/index.js');
-app.use( ruta);// entrega la ruta de la pagina web
- //app.use(require('./routes/authentication'));
-// //app.use('/links', require('./routes/links'));
+// // app.use(bodyParser.urlencoded({extended: false}));
+// // app.use(bodyParser.json());//interpreta estenciones Json
 
-
-module.exports = app;// al exportar app puede utlizar  toda la vicion del app ya que el mismo app es express
+// //app.use(multer().single('image'))//identifica si es una imagen que se envia al server antraves de 'image' para asi colocarla
+// 
